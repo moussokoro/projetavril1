4230fa77b9a14c1ec5ea74503b2f071d5b9801b3
moussokoro <47088761+moussokoro@users.noreply.github.com>
2019-04-16 12:12:36 +0200

    Add files via upload

---
+++ projetavril/projetavril/maison.php
@@ -0,0 +1,167 @@
+<!DOCTYPE html>
+<html>
+<head>
+  <meta charset="utf-8">
+  <title>maison</title>
+  <link rel="stylesheet" type="text/css" href="css/bootstrap.min.css">
+  <link rel="stylesheet" type="text/css" href="js/bootstrap.min.js">
+  <link rel="stylesheet" type="text/css" href="styles.css">
+</head>
+<body>
+
+<marquee><img src="images/logs.jpg">MERCI DE BIEN VOULOIR ACHETER OU LOUER UNE MAISON CHEZ MARIKO IMMOBILIER<img src="images/logs.jpg"></marquee>
+        
+      
+       
+        <section id="master">
+          <div class="col-md-3 col-sm-6 ">
+      <div class="accomd-modations-room">
+        <div class="img">
+          <img src="images/image2.jpg"  alt="" width="720" height="430">
+        </div>
+        <div class="text">
+          <h2>
+            <a href="connexion.php">ETAGE 5PCS</a>
+          </h2>
+
+            <p class="price">
+            <span class="amount">
+              <span class="awebooking-price ">
+                <span class="awebooking-price__amount">180.00FCFA</span></span></span>/mois</p>
+                  </div>
+      </div>
+    </div>
+
+
+   <div class="col-md-3 col-sm-6 ">
+      <div class="accomd-modations-room">
+        <div class="img">
+          <img src="images/ima.jpg"  alt="" width="720" height="430">
+        </div>
+        <div class="text">
+          <h2>
+            <a href="connexion.php">ETAGE 3PCS</a>
+          </h2>
+
+            <p class="price">
+            <span class="amount">
+              <span class="awebooking-price ">
+                <span class="awebooking-price__amount">150.000FCFA</span></span></span>/mois</p>
+                  </div>
+      </div>
+    </div>
+
+   <div class="col-md-3 col-sm-6 ">
+      <div class="accomd-modations-room">
+        <div class="img">
+          <img src="images/im.jpg"  alt="" width="720" height="430">
+        </div>
+        <div class="text">
+          <h2>
+            <a href="connexion.php">ETAGE 4PCS</a>
+          </h2>
+
+            <p class="price">
+            <span class="amount">
+              <span class="awebooking-price ">
+                <span class="awebooking-price__amount">150.00 FCFA</span></span></span>/mois</p>
+                  </div>
+      </div>
+    </div>
+
+   <div class="col-md-3 col-sm-6 ">
+      <div class="accomd-modations-room">
+        <div class="img">
+          <img src="images/image1.jpg"  alt="" width="720" height="430">
+        </div>
+        <div class="text">
+          <h2>
+            <a href="connexion.php">ETAGE 4PCS</a>
+          </h2>
+
+            <p class="price">
+            <span class="amount">
+              <span class="awebooking-price ">
+                <span class="awebooking-price__amount">130.00 FCFA</span></span></span>/mois</p>
+                  </div>
+      </div>
+    </div>
+
+
+
+
+        </section>
+        <section id="master">
+          <div class="col-xs-6">
+      <div class="accomd-modations-room">
+        <div class="img">
+          <img src="images/listroom_1-720x430.jpg"  alt="" width="720" height="430" >
+        </div>
+        <div class="text">
+          <h2>
+            <a href="connexion.php">VILLA  8PCS</a>
+          </h2>
+
+            <p class="price">
+            <span class="amount">
+              <span class="awebooking-price ">
+                <span class="awebooking-price__amount">150.00 FCFA</span></span></span>/mois</p>
+                  </div>
+      </div>
+    </div>
+    <div class="col-xs-6">
+      <div class="accomd-modations-room">
+        <div class="img">
+    <img src="images/room-1.jpg" class="attachment-awe-room-slider size-awe-room-slider wp-post-image" alt="" width="720" height="430"></a>
+        </div>
+        <div class="text">
+          <h2>
+            <a href="connexion.php">DUPLEXE 3pièces</a>
+          </h2>
+
+          <p class="price">
+          <span class="amount"><span class="awebooking-price ">
+            <span class="awebooking-price__amount">80.000FCFA</span></span></span>/mois          
+          </p>
+          </div>
+      </div>
+    </div>
+
+        <div class="col-xs-6">
+      <div class="accomd-modations-room">
+        <div class="img">
+          <a href="connexion.php">
+            <img src="images/room-3-720x430.jpg" class="attachment-awe-room-slider size-awe-room-slider wp-post-image" alt="" width="720" height="430"></a>
+        </div>
+        <div class="text">
+          <h2>
+            <a href="connexion.php">SUITE</a>
+          </h2>
+
+          <p class="price">
+          <span class="amount"><span class="awebooking-price ">
+          <span class="awebooking-price__amount">200.00FCFA</span></span></span>/mois </p>
+          </div>
+      </div>
+    </div>
+    <div class="col-xs-6">
+      <div class="accomd-modations-room">
+        <div class="img">
+          
+        <img src="images/thumbnail-720x430.jpg" class="attachment-awe-room-slider size-awe-room-slider wp-post-image" alt="" width="720" height="430"></a>
+        </div>
+        <div class="text">
+          <h2>
+            <a href="connexion.php">DUPLEXE 4PCS</a>
+          </h2>
+
+            <p class="price">
+              <span class="amount">
+                <span class="awebooking-price ">
+                <span class="awebooking-price__amount">160.00FCFA</span></span></span>/mois </p>
+      </div>
+      </div>
+    </div>  
+    </section >
+</body>
+</html>>
